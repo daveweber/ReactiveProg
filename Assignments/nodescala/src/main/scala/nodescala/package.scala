0e477df09576b567d375575eab85205d31259245
Dave Weber <weberd09@gmail.com>
2013-11-27 14:13:49 -0500

    Continue and ContinueWith implementations

--- Assignments/nodescala/src/main/scala/nodescala/package.scala
+++ Assignments/nodescala/src/main/scala/nodescala/package.scala
@@ -96,13 +96,21 @@ package object nodescala {
       case false => throw new NoSuchElementException
     }
 
-    /** Continues the computation of this future by taking the current future
+    /**
+     * Continues the computation of this future by taking the current future
      *  and mapping it into another future.
-     * 
+     *
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continueWith[S](cont: Future[T] => S): Future[S] = ???
+    def continueWith[S](cont: Future[T] => S): Future[S] = {
+      val p = Promise[S]()
+      f onComplete {
+        case Success(value) => p.complete(Try(cont(Future(value))))
+        case Failure(exception) => p.failure(exception)
+      }
+      p.future
+    }
 
     /** Continues the computation of this future by taking the result
      *  of the current future and mapping it into another future.
@@ -110,7 +118,14 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continue[S](cont: Try[T] => S): Future[S] = ???
+    def continue[S](cont: Try[T] => S): Future[S] = {
+      val p = Promise[S]()
+      f onComplete {
+        case Success(value) => p.complete(Try(cont(Try(value))))
+        case Failure(exception) => p.failure(exception)
+      }
+      p.future
+    }
 
   }
 
